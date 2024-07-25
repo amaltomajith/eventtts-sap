e7b545b771353c9c5d7d72197be160162f9402a3
George Bobby <mailto.georgebobby@gmail.com>
2024-07-25 10:49:43 +0530

    db connection

--- lib/dbconnection.ts
+++ lib/dbconnection.ts
@@ -1,25 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let isConnected = false;
 
 export const connectToDatabase = async () => {
+	if (!process.env.MONGODB_URI) {
+		throw new Error("Missing MONGODB_URI environment variable");
+	}
 
-    if (!process.env.MONGODB_URI) {
-        throw new Error('Missing MONGODB_URI environment variable');
-    }
+	if (isConnected) {
+		console.log("Using existing database connection");
+		return;
+	}
 
-    if (isConnected) {
-        console.log('Using existing database connection');
-        return;
-    }
+	try {
+		await mongoose.connect(process.env.MONGODB_URI, { dbName: "" });
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, { dbName: "evently" });
+		isConnected = true;
 
-        isConnected = true;
-
-        console.log('Connected to database');
-    } catch (error: any) {
-        throw new Error('Unable to connect to database', error);
-    }
-}
+		console.log("Connected to database");
+	} catch (error: any) {
+		throw new Error("Unable to connect to database", error);
+	}
+};
