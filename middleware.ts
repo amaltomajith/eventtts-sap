b1f4b700338f68d82db80fd4848d317249ffdba3
George Bobby <mailto.georgebobby@gmail.com>
2024-07-30 12:51:54 +0530

    package updates

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,24 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-	publicRoutes: [
-		"/",
-		"/event/:id",
-		"/api/webhook/clerk",
-		"/api/webhook/stripe",
-		"/api/uploadthing",
-	],
-	ignoredRoutes: [
-		"/api/webhook/clerk",
-		"/api/webhook/stripe",
-		"/api/uploadthing",
-	],
+const isPublicRoute = createRouteMatcher([
+	"/",
+	"/event/:id",
+	"/api/webhook/clerk",
+	"/api/webhook/stripe",
+	"/api/uploadthing",
+]);
+
+export default clerkMiddleware((auth, req) => {
+	if (!isPublicRoute(req)) {
+		auth().protect();
+	}
 });
 
 export const config = {
-	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		// Always run for API routes
+		"/(api|trpc)(.*)",
+	],
 };
