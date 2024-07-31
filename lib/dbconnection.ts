9c925a41ac6ee45707eb05cb9ab2eebb0e2a15b9
George Bobby <mailto.georgebobby@gmail.com>
2024-07-31 21:02:58 +0530

    db

--- lib/dbconnection.ts
+++ lib/dbconnection.ts
@@ -13,7 +13,7 @@ export const connectToDatabase = async () => {
 	}
 
 	try {
-		await mongoose.connect(process.env.MONGODB_URI, { dbName: "" });
+		await mongoose.connect(process.env.MONGODB_URI, { dbName: "eventtts" });
 
 		isConnected = true;
 
